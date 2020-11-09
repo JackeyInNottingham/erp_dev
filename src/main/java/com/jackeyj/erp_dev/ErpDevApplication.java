332d223d48adae5f1d9237dcbb346a39d7950bf6
Yaofei Ji <yaofei_ji@126.com>
2020-11-09 13:56:24 +0800

    first commit

---
+++ src/main/java/com/jackeyj/erp_dev/ErpDevApplication.java
@@ -0,0 +1,13 @@
+package com.jackeyj.erp_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jackeyj/erp_dev/ErpDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackeyj.erp_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
